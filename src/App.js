76ff4154eb202dbe57d5011c458220e98fcc7954
Hoai pham <hoai.pham@blueotter.net>
2022-05-22 14:44:50 +0700

    Init redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Board from 'Components/Board';
 import Heading from 'Components/Heading';
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
-  const [board, setBoard] = useState([
-    '', '', '', '', '',
-    '', '', '', '', '',
-    '', '', '', '', '',
-    '', '', '', '', '',
-    '', '', '', '', '',
-    '', '', '', '', ''
-  ])
+  const board = useSelector((state) => state.board.board);
   return (
     <div className="App">
       <Heading type="h1" text="Wordle" />
